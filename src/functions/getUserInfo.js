8dfb45d924586af6ccb945e76da55d85e85ad037
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2022-07-11 18:02:52 +1200

    Added a few functions

---
+++ src/functions/getUserInfo.js
@@ -0,0 +1,13 @@
+const {
+    SlashCommandBuilder
+} = require('@discordjs/builders');
+
+const {
+    MessageEmbed,
+} = require('discord.js');
+
+module.exports = {
+    async getUserInfo(client, interaction, cache) {
+
+    }
+}
