ab7da880d65416307ba4ed406d1d4c3e962282a2
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2022-07-11 17:39:27 +1200

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// import requirements 
+const {
+    Client,
+    Intents,
+    MessageEmbed,
+} = require('discord.js');
+
+// starting in djs v13, we are required to specify which intents we are using in the client constructor
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+const dotenv = require('dotenv');
+// import config IDs
+dotenv.config()
+const TOKEN = process.env.TOKEN
+
+const startup = require('./src/startup');
+// run this script upon starting up the bot and pass in the client
+startup(client)
+
+var cache = new Map();
+
+client.on('interactionCreate', async (interaction) => {
+    // if the interaction is not a command, eg: it's a button, return
+    if(!interaction.isCommand) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if(!command) return;
+
+    try {
+        return await command.execute(client, interaction, cache);
+
+    } catch(err) {
+        if(err) console.log(err);
+        await interaction.reply({
+            content: 'An error was detected, system has shut down temporarily.',
+            ephemeral: true
+        });
+    }
+
+});
+
+client.login(TOKEN);
---
+++ src/startup.js
@@ -0,0 +1,50 @@
+const { Collection } = require('discord.js');
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('discord-api-types/v9');
+
+const TOKEN = process.env.TOKEN
+const CLIENT_ID = process.env.CLIENT_ID
+const GUILD_ID = process.env.GUILD_ID
+
+const fs = require('fs')
+
+module.exports = (client) => {
+
+    const commands = [];
+    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+    // Creating a collection for commands in client
+    client.commands = new Collection();
+
+    for (const file of commandFiles) {
+        const command = require(`../commands/${file}`);
+        commands.push(command.data.toJSON());
+        client.commands.set(command.data.name, command);
+    }
+
+    console.log(client.commands);
+    console.log(commandFiles);
+
+    // When the client is ready, this only runs once
+    client.once('ready', async () => {
+        console.log(`${client.user.tag} is now online!`);
+
+        // Registering the commands in the client
+        const rest = new REST({
+            version: '9'
+        }).setToken(TOKEN);
+        (async () => {
+            try {
+                await rest.put(
+                    Routes.applicationCommands(CLIENT_ID), {
+                    body: commands
+                },
+                );
+                console.log('Successfully registered application commands globally');
+            } catch (error) {
+                if (error) console.error(error);
+            }
+        })();
+
+    });
+
+}
