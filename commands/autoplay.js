2f0d8da08f950293ade062198c81bf1a3f078564
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2022-07-25 18:03:43 +1200

    Made autoplay!

--- commands/autoplay.js
+++ commands/autoplay.js
@@ -7,6 +7,8 @@ const {
     MessageEmbed,
 } = require('discord.js');
 
+const vars = require('../variables.json');
+
 module.exports = {
 
     data: new SlashCommandBuilder()
@@ -29,11 +31,29 @@ module.exports = {
         var serverQueue = cache.get(interaction.guild.id);
 
         if (interaction.options.getSubcommand() === 'on') {
+            try {
+                serverQueue.autoplay = true;
+            } catch(e) {
+                console.log(e);
+            }
 
+            const emb = new MessageEmbed()
+                .setAuthor({ name: "I have turned autoplay on for this session", iconURL: interaction.member.user.avatarURL(), url: 'https://discord.gg/GyGCYu5ukJ' })
+                .setColor(vars.successColour)
+            await interaction.reply({ embeds: [emb] });
         }
 
         if (interaction.options.getSubcommand() === 'off') {
+            try {
+                serverQueue.autoplay = false;
+            } catch(e) {
+                console.log(e);
+            }
 
+            const emb = new MessageEmbed()
+                .setAuthor({ name: "I have turned autoplay off for this session", iconURL: interaction.member.user.avatarURL(), url: 'https://discord.gg/GyGCYu5ukJ' })
+                .setColor(vars.successColour)
+            await interaction.reply({ embeds: [emb] });
         }
 
     }
