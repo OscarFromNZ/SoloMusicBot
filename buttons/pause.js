4dd6c01f8114ca7683428c0550197bde278a9423
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2022-08-01 18:30:44 +1200

    Added try command to the pause button

--- buttons/pause.js
+++ buttons/pause.js
@@ -8,11 +8,16 @@ const vars = require('../variables.json');
 module.exports = {
     async execute(client, interaction, cache, audio) {
         if ( !audio ) {
+            console.log("Audio is not defined.!");
             audio = new Map();
         }
 
-        let player = audio.get(interaction.guild.id);
-        player.pause();
+        try {
+            let player = audio.get(interaction.guild.id);
+            player.pause();
+        } catch (err) {
+            console.log(err)
+        }
 
         const emb = new MessageEmbed()
             .setAuthor({ name: "Paused the song, to unpause, run /unpause", iconURL: interaction.member.user.avatarURL(), url: 'https://discord.gg/GyGCYu5ukJ' })
