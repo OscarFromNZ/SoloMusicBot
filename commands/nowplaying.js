b04ecbc542b0085f458423da877690456d04f91e
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2022-08-01 21:03:13 +1200

    Create nowplaying.js

---
+++ commands/nowplaying.js
@@ -0,0 +1,33 @@
+const {
+    SlashCommandBuilder
+} = require('@discordjs/builders');
+
+const {
+    MessageEmbed,
+} = require('discord.js');
+
+const vars = require('../variables.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('nowplaying')
+        .setDescription('🎵 View information of the current song'),
+
+    async execute(client, interaction, cache) {
+        const serverQueue = cache.get(interaction.guild.id);
+        const songs = serverQueue.songs;
+
+        if (!songs[0]) {
+            const emb = new MessageEmbed()
+                .setAuthor({ name: "I am not playing any songs right now", iconURL: interaction.member.user.avatarURL(), url: 'https://discord.gg/GyGCYu5ukJ' })
+                .setColor(vars.dangerColour)
+            await interaction.reply({ embeds: [emb] });
+        }
+
+        const emb = new MessageEmbed()
+            .setAuthor({ name: "I am currently playing \"" + songs[0].video_details.title + "\" by " + songs[0].video_details.channel, iconURL: interaction.member.user.avatarURL(), url: 'https://discord.gg/GyGCYu5ukJ' })
+            .setColor(vars.successColour)
+        await interaction.reply({ embeds: [emb] });
+    }
+
+}
