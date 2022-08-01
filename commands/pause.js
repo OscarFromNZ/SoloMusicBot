8e023c50c620591dd340ca3a16a23a68b649adaf
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2022-08-01 18:26:37 +1200

    Removed Mercy message

--- commands/pause.js
+++ commands/pause.js
@@ -23,7 +23,6 @@ module.exports = {
         const emb = new MessageEmbed()
             .setAuthor({ name: "Paused the song, to unpause, run /unpause", iconURL: interaction.member.user.avatarURL(), url: 'https://discord.gg/GyGCYu5ukJ' })
             .setColor(vars.successColour)
-            .setFooter({ text: "This command is quite new and buggy, you can always mute me instead! :)"})
         await interaction.reply({ embeds: [emb] });
     }
 
