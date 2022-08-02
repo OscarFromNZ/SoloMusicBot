11e0f3312111b1e4dcc18997f15aa584ba2971b4
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2022-08-02 17:39:16 +1200

    Summary (required)

--- src/functions/playSong.js
+++ src/functions/playSong.js
@@ -35,9 +35,9 @@ module.exports = {
                 noSubscriber: NoSubscriberBehavior.Play
             }
         });
-        // 
+        
         audio.set(interaction.guild.id, player);
-        // 
+        
         console.log("Player initialized and ready");
 
         let source = await play.stream(url);
