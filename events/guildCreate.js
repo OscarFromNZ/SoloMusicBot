a4f5cb4be93c83b05c552309e3dcb733a1abc3b3
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2022-08-08 18:21:17 +1200

    Commit

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -13,7 +13,7 @@ module.exports = {
 		const emb = new MessageEmbed()
 			.setTitle("<:solo:991232706274926683> Solo, the top music bot for Discord!")
 			.setDescription("Thank you for adding me to your server! I am an advanced music bot where you can listen to music bot multiple streaming platforms **without ads**, **with friends**, on Discord.\n\nTo give me whirl, you can run the following commands!\nㅤ</join:1005312384442839071> - Join me to your current voice channel!\nㅤ</play:1005312686084608040> - Play a song into your voice channel!\n\nIf you ever need help with the bot, you can run the following commands!\nㅤ</help:1005313529475256413> - Join me to your current voice channel!\nㅤ</support:1005314475689254932> - Get a link to our Discord support server!")
-			.setImage("https://user-images.githubusercontent.com/93412639/183234556-ecf2b0b0-2577-410c-a30a-e6a9a8d01092.gif")
+			.setImage("https://i.imgur.com/atEz3Js.gif")
 
 		channel.send( { embeds: [emb] } );
 		
