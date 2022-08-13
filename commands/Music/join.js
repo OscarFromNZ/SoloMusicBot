761f0ceb892a0c6b8349597d46a3b1e622ecf649
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2022-08-13 21:16:58 +1200

    Added try statements

--- commands/Music/join.js
+++ commands/Music/join.js
@@ -20,55 +20,59 @@ module.exports = {
 
         console.log("\x1b[36m%s\x1b[0m", "Ran " + interaction.commandName + " command");
 
-        console.log("Beginnng checks");
-        const permissions = await interaction.member.voice.channel.permissionsFor(client.user.id);
-        if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
-            const emb = new MessageEmbed()
-                .setAuthor({ name: "I do not have permission to join or speak in this channel", iconURL: interaction.member.user.avatarURL(), url: 'https://discord.gg/GyGCYu5ukJ' })
-                .setColor(vars.dangerColour)
-            await interaction.editReply({ embeds: [emb] });
-            return;
-        }
-
-        console.log("Passed permissions check");
-        if (interaction.guild.me.voice.channel) {
-
-            console.log("Getting connection");
-            let connection = getVoiceConnection();
-            console.log("Got connection");
-            if (typeof connection == 'undefined') return interaction.editReply({ content: "An error occurred, get a mod to disconnect me manually and then run join again \n (I have no idea how you managed to get this error lol)"});
-            console.log("Connection is not undefined");
-
-            const emb = new MessageEmbed()
-                .setAuthor({ name: "I am already in a voice channel :(", iconURL: interaction.member.user.avatarURL(), url: 'https://discord.gg/Rkq2f3b8Tn'})
-                .setColor(vars.dangerColour)
-            await interaction.editReply({ embeds: [emb] });
-            return;
-        }
-        console.log("Passed VC check");
-        console.log("Passed checks");
-
-        const guild = client.guilds.fetch(interaction.member.guild.id).then(guild => {
-            const channel = guild.channels.fetch(interaction.member.voice.channel.id).then(channel => {
-                console.log("🌳 Joining VC");
-                const connection = joinVoiceChannel({
-                    guildId: guild.id,
-                    channelId: channel.id,
-                    adapterCreator: guild.voiceAdapterCreator
-                })
-                console.log("🌳 Joined VC");
-
+        try {
+            console.log("Beginnng checks");
+            const permissions = await interaction.member.voice.channel.permissionsFor(client.user.id);
+            if (!permissions.has("CONNECT") || !permissions.has("SPEAK")) {
                 const emb = new MessageEmbed()
-                    .setAuthor({ name: "Joined your current voice channel", iconURL: interaction.member.user.avatarURL(), url: 'https://discord.gg/Rkq2f3b8Tn'})
-                    .setColor(vars.successColour)
-                interaction.editReply({ embeds: [emb], content: "🎶 **Tip:** Use </play:1005558358604009472> to queue a song!" })
-
-                // setting the connection to the serverqueue object so we can access it later
-                let serverQueue = cache.get(interaction.guild.id);
-                serverQueue.connection = connection;
-                cache.set(serverQueue, interaction.guild.id);
+                    .setAuthor({ name: "I do not have permission to join or speak in this channel", iconURL: interaction.member.user.avatarURL(), url: 'https://discord.gg/GyGCYu5ukJ' })
+                    .setColor(vars.dangerColour)
+                await interaction.editReply({ embeds: [emb] });
+                return;
+            }
+    
+            console.log("Passed permissions check");
+            if (interaction.guild.me.voice.channel) {
+    
+                console.log("Getting connection");
+                let connection = getVoiceConnection();
+                console.log("Got connection");
+                if (typeof connection == 'undefined') return interaction.editReply({ content: "An error occurred, get a mod to disconnect me manually and then run join again \n (I have no idea how you managed to get this error lol)"});
+                console.log("Connection is not undefined");
+    
+                const emb = new MessageEmbed()
+                    .setAuthor({ name: "I am already in a voice channel :(", iconURL: interaction.member.user.avatarURL(), url: 'https://discord.gg/Rkq2f3b8Tn'})
+                    .setColor(vars.dangerColour)
+                await interaction.editReply({ embeds: [emb] });
+                return;
+            }
+            console.log("Passed VC check");
+            console.log("Passed checks");
+    
+            const guild = client.guilds.fetch(interaction.member.guild.id).then(guild => {
+                const channel = guild.channels.fetch(interaction.member.voice.channel.id).then(channel => {
+                    console.log("🌳 Joining VC");
+                    const connection = joinVoiceChannel({
+                        guildId: guild.id,
+                        channelId: channel.id,
+                        adapterCreator: guild.voiceAdapterCreator
+                    })
+                    console.log("🌳 Joined VC");
+    
+                    const emb = new MessageEmbed()
+                        .setAuthor({ name: "Joined your current voice channel", iconURL: interaction.member.user.avatarURL(), url: 'https://discord.gg/Rkq2f3b8Tn'})
+                        .setColor(vars.successColour)
+                    interaction.editReply({ embeds: [emb], content: "🎶 **Tip:** Use </play:1005558358604009472> to queue a song!" })
+    
+                    // setting the connection to the serverqueue object so we can access it later
+                    let serverQueue = cache.get(interaction.guild.id);
+                    serverQueue.connection = connection;
+                    cache.set(serverQueue, interaction.guild.id);
+                })
             })
-        })
+        } catch (e) {
+            console.log(e);
+        }
 
     }
 
--- commands/Music/lyrics.js
+++ commands/Music/lyrics.js
@@ -15,6 +15,11 @@ module.exports = {
 
     async execute(client, interaction, cache) {
         console.log("\x1b[36m%s\x1b[0m", "Ran " + interaction.commandName + " command");
+        try {
+            await interaction.editReply("Sorry, this command has not been made yet~ This bot is extremely new");
+        } catch (e) {
+            console.log(e)
+        }
     }
 
 }
